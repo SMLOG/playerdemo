d618f102c148f942f5cc6209ef709871bad745d8
alex <alex@youremail>
2022-08-04 16:14:14 +0800

    upadte

---
+++ app/src/main/java/com/shuyu/simple/video/NormalGSYVideoPlayer.java
@@ -0,0 +1,47 @@
+package com.shuyu.simple.video;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.shuyu.gsyvideoplayer.R;
+import com.shuyu.simple.video.base.GSYVideoView;
+
+/**
+ * Created by guoshuyu on 2017/4/1.
+ * 使用正常播放按键和loading的播放器
+ */
+
+public class NormalGSYVideoPlayer extends StandardGSYVideoPlayer {
+
+    public NormalGSYVideoPlayer(Context context, Boolean fullFlag) {
+        super(context, fullFlag);
+    }
+
+    public NormalGSYVideoPlayer(Context context) {
+        super(context);
+    }
+
+    public NormalGSYVideoPlayer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.video_layout_normal;
+    }
+
+    @Override
+    protected void updateStartImage() {
+        if(mStartButton instanceof ImageView) {
+            ImageView imageView = (ImageView) mStartButton;
+            if (mCurrentState == GSYVideoView.CURRENT_STATE_PLAYING) {
+                imageView.setImageResource(R.drawable.video_click_pause_selector);
+            } else if (mCurrentState == GSYVideoView.CURRENT_STATE_ERROR) {
+                imageView.setImageResource(R.drawable.video_click_play_selector);
+            } else {
+                imageView.setImageResource(R.drawable.video_click_play_selector);
+            }
+        }
+    }
+}
